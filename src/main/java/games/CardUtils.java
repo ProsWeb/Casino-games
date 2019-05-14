bfa00357391bdc1d1bf90c1d9e491030c8d78757
ProsWeb <pros15@mail.ru>
2019-05-14 15:47:27 +0500

    add cardUtils

---
+++ src/main/java/games/CardUtils.java
@@ -0,0 +1,55 @@
+package games;
+
+import org.apache.commons.math3.util.MathArrays;
+
+class CardUtils {
+
+    private static final int PARS_TOTAL_COUNT = Par.values().length;
+
+    static final int CARDS_TOTAL_COUNT = PARS_TOTAL_COUNT * Suit.values().length;
+
+    enum Suit {
+
+        SPADES, // пики
+        HEARTS, // червы
+        CLUBS, // трефы
+        DIAMONDS // бубны
+
+    }
+
+    enum Par {
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK, // Валет
+        QUEEN, // Дама
+        KING, // Король
+        ACE // Туз
+    }
+
+    private static Suit getSuit(final int cardNumber) {
+        return Suit.values()[cardNumber / PARS_TOTAL_COUNT];
+    }
+
+    static Par getPar(final int cardNumber) {
+        return Par.values()[cardNumber % PARS_TOTAL_COUNT];
+    }
+
+    static String toString(final int cardNumber) {
+        return getPar(cardNumber) + " " + getSuit(cardNumber);
+    }
+
+    static int[] getShuffledCards() {
+        // колода подряд
+        int[] cards = {
+                0, 1, 2, 3, 4, 5, 6, 7, 8,  // бубны
+                9, 10, 11, 12, 13, 14, 15, 16, 17,  // червы
+                18, 19, 20, 21, 22, 23, 24, 25, 26,  // трефы
+                27, 28, 29, 30, 31, 32, 33, 34, 35}; // пики
+
+        MathArrays.shuffle(cards);
+        return cards;
+    }
+}
